aca9239c82c4a9ea3cfa3d847017cb178911f9aa
Marek Nawrocki <aldhafara@onet.pl>
2025-06-03 18:24:28 +0200

    Add Item entity

---
+++ src/main/java/com/aldhafara/useritemservice/entities/Item.java
@@ -0,0 +1,38 @@
+package com.aldhafara.useritemservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(User owner, String name) {
+        this.owner = owner;
+        this.name = name;
+    }
+}
