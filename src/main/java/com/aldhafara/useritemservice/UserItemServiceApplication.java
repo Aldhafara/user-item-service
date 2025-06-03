74843be9fcf3e57a53d5580e52b6e8876e6bfbc9
Marek Nawrocki <aldhafara@onet.pl>
2025-06-03 12:25:43 +0200

    Add initial Spring Boot project structure

---
+++ src/main/java/com/aldhafara/useritemservice/UserItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aldhafara.useritemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserItemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldhafara/useritemservice/UserItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldhafara.useritemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
